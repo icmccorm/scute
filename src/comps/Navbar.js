89e70b9f5923cbc8fa731c8175bef4db335e1180
Ian McCormack <ian.colcanon@gmail.com>
2019-06-01 22:11:49 -0500

    Updated lexer with ES6 syntax and made core functions independent of source text.

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import '../css/Navbar.css';
 import Button from './Button';
-class Navbar extends React.Component { 
+import Lexer from '../lang/Lexer'
+
+class Navbar extends Component { 
     
     constructor(props){
         super(props);
@@ -12,12 +14,22 @@ class Navbar extends React.Component {
         return (
             <div className='navbar sky-b'>
                 <div className='nav nav-l'>
-                    <Button title='Run'/>
-                    <Button title='Run'/>
-                    <Button title='Run'/>
+                    <Button title='Run' click={this.compile}/>
+
                 </div>   
             </div>
         );
+    }
+
+    compile () {
+        console.log("Click");
+        Lexer.run("henlo", 1).then(tokens => {
+            console.log(JSON.stringify(tokens));
+        });
+        Lexer.run("whats up", 2).then(tokens => {
+            console.log(JSON.stringify(tokens));
+        });
+        
     }
 }
 export default Navbar;
