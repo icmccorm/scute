fa490b492768ccd6c4eeb4628d37bf4ff3d1e213
Ian McCormack <ian.colcanon@gmail.com>
2019-08-06 20:31:57 -0500

    Reverted back to babel-loader  with different browserslists between prod and dev, and ignored /dist.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,25 +1,11 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const paths = require('./paths.js');
+
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist'
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                include: paths.src,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.tsx?$/,
-                exclude: [/node-modules/, /\.(js|jsx)$/],
-                use: 'ts-loader',
-            }
-        ],
-    },
-
 })
