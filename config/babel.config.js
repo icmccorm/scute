9b3a87cf7cc7f3eb18d4cfd386db19371c8a8385
Ian McCormack <ian.colcanon@gmail.com>
2019-08-10 11:20:35 -0500

    Added object rest/spread support to babel.

--- config/babel.config.js
+++ config/babel.config.js
@@ -13,7 +13,8 @@ module.exports = function (api) {
       ];
     
     const plugins = [
-     '@babel/plugin-proposal-class-properties'
+     '@babel/plugin-proposal-class-properties',
+     '@babel/plugin-proposal-object-rest-spread'
     ];
 
     return {
