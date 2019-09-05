43ae34963b3fc930895dc5de802488fb22a2061c
Ian McCormack <ian.colcanon@gmail.com>
2019-09-05 15:21:45 -0500

    Configured string transfer from JS to wasm.

--- src/scute.worker.ts
+++ src/scute.worker.ts
@@ -1,7 +1,28 @@
 import InterpreterModule from './lang/scute';
 
-InterpreterModule().then(() => {
+InterpreterModule().then((em_module) => {
 	send('ready');
+	self.onmessage = event => {
+		switch(event.data){
+			default:
+				runCode(event.data);
+				break;
+		}
+	}
+
+	function runCode(string) {
+		let ptr = stringToCharPtr(string);
+		em_module.ccall('runCode', 'number', ['number'], [stringToCharPtr(ptr)]);
+		em_module._free(ptr);
+	}
+
+	function stringToCharPtr(s: string) {
+		let charArray = em_module.intArrayFromString(screenLeft);
+		let charArrayPtr = em_module._malloc(charArray.length*charArray.BYTES_PER_ELEMENT);
+		em_module.HEAPU8.set(charArray, charArrayPtr);
+		return charArrayPtr;
+	}
+
 })
 
 function send(s: string) {
