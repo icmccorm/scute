22e47a204609ec7c1ca6838065c1108fc287cae6
Ian McCormack <ian.colcanon@gmail.com>
2019-09-09 13:37:36 -0500

    Added dynamic shape creation and updating via Canvas component.

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,28 +1,58 @@
 import * as React from 'react';
 import './css/Canvas.css';
+import Shape from './Shape';
 
+
+type State = {tags: Tag[]}
 class Canvas extends React.Component<any, any> {
-    display: {
-        svg: string,
-        css: string
-    };
-
+    readonly state: State;
+    updates: Map<number, Function>;
     constructor(props: object){
         super(props);
-        this.state = {};
+        this.updates = new Map<number, Function>();
+        this.state = {tags: []}
+
+    }
+    update(id:number, tagName:string){
+        let updateFunction = this.updates.get(id);
+        if(updateFunction){
+            updateFunction(null);
+        }else{
+            this.setState({tags: this.state.tags.concat(new Tag(tagName, id))});
+        }
     }
 
+    componentDidMount(){
+        this.update(1, "rect");
+
+    }
     render () {
         return (
                 <div className= 'canvas shadow'>
                     <svg id='canvas'>
-                        {this.props.display.svg}
+                        {this.state.tags.map((item) => {
+                            return <Shape 
+                                        key={item.id}
+                                        tagName={item.tagName}
+                                        id={item.id}
+                                        funcMap={this.updates}
+                                        attrs={[]}
+                                    />
+                        })}
                     </svg>
-                    <style>
-                        {this.props.display.css}
-                    </style>
                 </div>
         );
     }
 }
+
+class Tag{
+    tagName: string;
+    id: number;
+    constructor(tagName, id){
+        this.tagName = tagName;
+        this.id = id;
+    }
+}
+
 export default Canvas;
+
